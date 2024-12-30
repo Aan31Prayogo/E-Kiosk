5159657ce0f69f44cdeb2853838e4edb5d5713ca
Yulian Surya Prayogo <yulian@cognizest.com>
2024-12-30 20:49:17 +0700

    fix : template card menu

--- UI/CardMenuTemplate.py
+++ UI/CardMenuTemplate.py
@@ -11,30 +11,32 @@ class CardMenuTemplate(ft.Card):
         self.image_url = json_data['image_url']
 
     def on_hover(self, e):
-        print(e.data)
-        e.control.border = ft.border.all(3, "amber") if e.data == "true" else ft.border.all(3, "transparent")
+        e.control.border = ft.border.all(1, "#edb009") if e.data == "true" else ft.border.all(3, "transparent")
         e.control.update()
 
     def build(self):
         try:
             self.card_content = ft.Container(
                 width=180,
-                height=200,
+                height=150,
                 padding=0,
                 border_radius=ft.border_radius.all(10),
                 border=ft.border.all(2, "#e4e4e4"),
-                ink=True,
+                ink=False,
                 on_hover=self.on_hover,
                 bgcolor="#e4e4e4",
                 content = ft.Column(
+                    spacing=0,
                     height=180, 
                     controls=[
                         ft.Image(
                             src=self.image_url,
                             width=200,
                             height=100,
+                            border_radius=ft.border_radius.all(20),
+
                         ),
-                        ft.Text(self.origin, color="#403f3c", size=12, text_align=ft.TextAlign.START),
+                        ft.Text(self.origin, color="#403f3c", size=12, text_align=ft.TextAlign.START, italic=True),
                         ft.Text(self.title, color="#403f3c", size=18, text_align=ft.TextAlign.START)
                     ],
                     alignment=ft.MainAxisAlignment.START
@@ -43,7 +45,7 @@ class CardMenuTemplate(ft.Card):
 
             self.card_menu = ft.Card(
                 content=self.card_content,
-                elevation=5.3
+                elevation=10
             )
 
             # print(self.title, self.origin, self.description, self.image_url)
